d830fbc55b9755d51f296e1f290fa6c4db05eff7
cell42 <teamcell42@gmail.com>
2017-09-15 11:43:49 +0200

    Update StreamingMediaProxy.js

--- src/windows/StreamingMediaProxy.js
+++ src/windows/StreamingMediaProxy.js
@@ -1,27 +1,31 @@
-"use strict";
-function StreamingMedia() {
-}
+    "use strict";
+    function StreamingMedia() {
+    }
 
-StreamingMedia.prototype.playVideo = function (url, options) {
-    var uri = new Windows.Foundation.Uri(url)
+    StreamingMedia.prototype.playVideo = function (url, options) {
+        this.__openFile(url);
+    };
 
-    Windows.Storage.StorageFile.getFileFromApplicationUriAsync(uri)
-    .done(function(filePath) {
-        Windows.System.Launcher.launchFileAsync(filePath);
-    });
-};
+    StreamingMedia.prototype.playAudio = function (url, options) {
+        this.__openFile(url);
+    };
 
-StreamingMedia.prototype.playAudio = function (url, options) {
-	throw new Error("InvalidFunction: playAudio is not yet implemented");
-};
+    StreamingMedia.prototype.install = function () {
+	    if (!window.plugins) {
+		    window.plugins = {};
+	    }
+	    window.plugins.streamingMedia = new StreamingMedia();
+	    return window.plugins.streamingMedia;
+    };
 
-StreamingMedia.prototype.install = function () {
-	if (!window.plugins) {
-		window.plugins = {};
-	}
-	window.plugins.streamingMedia = new StreamingMedia();
-	return window.plugins.streamingMedia;
-};
+    StreamingMedia.prototype.__openFile = function (url) {
+        var uri = new Windows.Foundation.Uri(url)
 
-cordova.addConstructor(StreamingMedia.prototype.install);
-cordova.commandProxy.add("StreamingMedia", StreamingMedia);
+        Windows.Storage.StorageFile.getFileFromApplicationUriAsync(uri)
+        .done(function(filePath) {
+            Windows.System.Launcher.launchFileAsync(filePath);
+        });
+    };
+
+    cordova.addConstructor(StreamingMedia.prototype.install);
+    cordova.commandProxy.add("StreamingMedia", StreamingMedia);
